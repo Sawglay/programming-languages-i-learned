e2c6cb2aa8149fe909fd1d04947313c90a609a15
G Lay <sawglay2@gmail.com>
2026-02-18 00:40:01 +0700

    first-commit

---
+++ if-else.py
@@ -0,0 +1,7 @@
+name = input("Enter your name : ")
+password = int(input("Enter your password_interger : "))
+
+if name == "glay" and password == 2003:
+    print("Oh, my master, welcome back!")
+else:
+    print("You are not allowed")
