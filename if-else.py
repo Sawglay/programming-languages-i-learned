4183414eaaee6543ba63dec3b585c2b68d10b014
G Lay <sawglay2@gmail.com>
2026-02-18 00:54:25 +0700

    fixing-changes-to-get-final-output

--- if-else.py
+++ if-else.py
@@ -3,5 +3,7 @@ password = int(input("Enter your password_interger : "))
 
 if name == "glay" and password == 2003:
     print("Oh, my master, welcome back!")
+elif name == "glay" or password == 2003:
+    print("Something is wrong! Try again.")
 else:
     print("You are not allowed")
