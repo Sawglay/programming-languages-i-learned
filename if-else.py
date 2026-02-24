168086eb0337e905ff3385e9b3850ae5210d171c
G Lay <sawglay2@gmail.com>
2026-02-24 20:24:55 +0700

    Added while-loop

--- if-else.py
+++ if-else.py
@@ -1,9 +1,19 @@
+# name = input("Enter your name : ")
+# password = int(input("Enter your password_interger : "))
+
+# if name == "glay" and password == 2003:
+#     print("Oh, my master, welcome back!")
+# elif name == "glay" or password == 2003:
+#     print("Something is wrong! Try again.")
+# else:
+#     print("You are not allowed")
+
 name = input("Enter your name : ")
-password = int(input("Enter your password_interger : "))
+password = int(input("Enter your password : "))
 
 if name == "glay" and password == 2003:
-    print("Oh, my master, welcome back!")
+    print("You are allowed to enter!")
 elif name == "glay" or password == 2003:
-    print("Something is wrong! Try again.")
+    print("Something went wrong!")
 else:
-    print("You are not allowed")
+    print("No, you are not allowed.")
