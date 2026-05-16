0c6aade751d133644d6b22d315e463dccb4a3c0b
Sawglay <sawglay2@gmail.com>
2026-05-16 13:07:18 +0700

    Updated Spread Operators and Destructuring

--- array.js
+++ array.js
@@ -30,3 +30,45 @@ arr[0][3] = "d"
 console.log(arr);
 
 let NNA = [[0,1,2],['a','x','d'],[12]]
+
+
+
+
+//spread operators = ...    //deleting [], without deleting the value in []
+
+let data1 = [1,2,3]
+let data2 = [4,5,6]
+let result = [...data1, ...data2]
+console.log(result);
+
+// function add(a,b){
+//     console.log(a+b);
+    
+// }
+
+// let num = [1,2];
+// add(...num);
+
+
+
+
+
+
+//destructuring 
+
+let data = ['MgMg', 22, "TTU"]
+// let name = data[0]
+// let age = data[1]
+// let school = data[2]
+// console.log(name, age, school);
+
+let [name, age, school] = data
+console.log(name, age, school);
+
+function add([a,b]){
+    console.log(a+b);
+    
+}
+
+add([1,2]);
+
